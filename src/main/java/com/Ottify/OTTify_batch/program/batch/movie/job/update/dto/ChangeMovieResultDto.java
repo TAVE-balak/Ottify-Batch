d41b880905272ed32d5ed89ca1b59ff5a1767822
Jinu Yi <dionisos198@naver.com>
2025-01-18 22:58:55 +0900

    Feat: update movie job

---
+++ src/main/java/com/Ottify/OTTify_batch/program/batch/movie/job/update/dto/ChangeMovieResultDto.java
@@ -0,0 +1,9 @@
+package com.Ottify.OTTify_batch.program.batch.movie.job.update.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ChangeMovieResultDto {
+    private Long id;
+    private boolean adult;
+}
